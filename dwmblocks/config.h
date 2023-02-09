0e1ce675269cb1652be56dc768f5c0830242cdfe
meliodas0n <mrunalnarayana@gmail.com>
2023-02-09 22:25:43 +0530

    made some changes

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -3,12 +3,8 @@ static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"",  "nettraf",  1,  16}, 
-  {"",  "volume", 1,  10},
   {"",  "clock",  60, 1},
-  {"",  "internet", 5,  4},
-  {"", "cpu", 1, 10},
   {"", "memory", 1, 10},
-  {"", "disk", 1, 10},
   {"", "battery", 1, 10},
 };
 
